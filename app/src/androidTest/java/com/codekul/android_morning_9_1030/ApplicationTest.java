e518965518b9878c37cc25208113bf7de386e509
aniruddha <aniruddha.kudalkar@gmail.com>
2016-08-29 09:49:04 +0530

    initial setup

---
+++ app/src/androidTest/java/com/codekul/android_morning_9_1030/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.codekul.android_morning_9_1030;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
